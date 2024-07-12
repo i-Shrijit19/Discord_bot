8bd9f4191dbccb4ab89b22ec32220652a0e9ff18
i-Shrijit19 <147278443+i-Shrijit19@users.noreply.github.com>
2024-07-13 03:17:13 +0530

    Add files via upload

---
+++ responses.py
@@ -0,0 +1,18 @@
+import random
+def get_response(user_input: str) -> str:
+    lowered: str = user_input.lower()
+
+    if lowered == '':
+        return 'Well, you\'re awfully silent...'
+    elif 'hello' in lowered:
+        return 'Hello there!'
+    elif 'how are you' in lowered:
+        return 'Good, thanks!'
+    elif 'bye' in lowered:
+        return 'See you!'
+    elif 'roll dice' in lowered:
+        return f'You rolled: {random.randrange(1, 6)}'
+    else:
+        return random.choice(['I do not understand...',
+                       'What are you talking about?',
+                       'Do you mind rephrasing that?'])
